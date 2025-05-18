49a748b3dbfda3c078aaf04e29996a8a868c7f0d
azizulhoq953 <azizulhoq2041@gmail.com>
2025-05-18 20:13:41 +0600

    first commit

---
+++ menstrual-tracker-backend/src/index.ts
@@ -0,0 +1,78 @@
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db";
+// import router from "./routes";
+// import path from "path";
+// // import { fileURLToPath } from "../";
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// // CORS middleware
+// const corsOptions = {
+//   origin: "http://localhost:3001", // Allow frontend running on port 3000
+//   methods: ["GET", "POST", "PUT", "DELETE"], // Allow GET and POST methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers (including Authorization)
+//   credentials: true, // Allow credentials (cookies, authorization headers)
+// };
+
+// app.use(cors(corsOptions));
+
+// // Body parsers
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// // Static file serving (uploads)
+// // Static file serving (uploads)
+// app.use("../uploads", express.static(path.join(__dirname, "../uploads")));
+
+// // API routes
+// app.use("/api", router);
+
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+import router from "./routes";
+import path from "path";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// CORS middleware
+const corsOptions = {
+  origin: "http://localhost:3000", // Allow frontend running on port 3001
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow GET and POST methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers (including Authorization)
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
+
+app.use(cors(corsOptions));
+
+// Body parsers
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+const uploadsDir = path.join(process.cwd(), 'uploads', 'ProductImage');
+app.use('/uploads', express.static(uploadsDir));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+// Static file serving (uploads)
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// API routes
+app.use("/api", router);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
